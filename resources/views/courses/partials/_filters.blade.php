de5841ed321b9a8568a69c54227ef87eff2eaa62
Mohamed Allam <meda3gm@gmail.com>
2018-05-28 16:19:26 +0100

    refactoring filters further more and clear filter fix

--- resources/views/courses/partials/_filters.blade.php
+++ resources/views/courses/partials/_filters.blade.php
@@ -1,8 +1,14 @@
-<p class="list-group">
-    <a href="{{ route('courses.index') }}">Clear All</a>
-</p>
+@php
+    $mappings = App\Filters\Course\CourseFilters::mappings();
+@endphp
 
-@foreach(App\Filters\Course\CourseFilters::mappings() as $key => $map)
+@if (count(array_intersect(array_keys(request()->query()), array_keys($mappings))))
+    <p>
+        <a href="{{ route('courses.index') }}">Clear all filters</a>
+    </p>
+@endif
+
+@foreach($mappings as $key => $map)
     @include('courses.partials._filter_list', compact('key', 'map'))
 @endforeach
 
