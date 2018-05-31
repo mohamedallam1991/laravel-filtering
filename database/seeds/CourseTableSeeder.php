10cb8703183c0ec794e268149054d765d3d32ab1
Mohamed Allam <meda3gm@gmail.com>
2018-05-31 02:21:03 +0100

    seeding subjects and courses

--- database/seeds/CourseTableSeeder.php
+++ database/seeds/CourseTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CourseTableSeeder extends Seeder
 {
@@ -11,6 +12,25 @@ class CourseTableSeeder extends Seeder
      */
     public function run()
     {
-        factory('App\Course', 100)->create();
+
+    $subjectsss = ['php', 'laravel', 'vue', 'redis', 'javascript', 'mongodb', 'nodejs', 'vim', 'fedora'];
+    foreach($subjectsss as $key => $value )
+    {
+        DB::table('subjects')->insert(['name' => strtoupper($value), 'slug' => $value]); 
+    }
+
+    //return \App\Subject::get();
+    
+
+    $subjects = \App\Subject::get();
+
+    foreach ($subjects as $subject)
+    {
+        $courses = factory('App\Course', 10)->create()->each(function ($courses) use ($subject){
+            $courses->subjects()->save($subject);
+        });
+    }
+
+        factory('App\Course', 50)->create();
     }
 }
