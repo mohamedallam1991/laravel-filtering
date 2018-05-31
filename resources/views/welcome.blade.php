1aa23ca4a07e23a4b0e4b3d5bac80f23e438a264
Mohamed Allam <meda3gm@gmail.com>
2018-05-31 02:24:57 +0100

    link in welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                <a href="/courses">Click me to check courses</a>
                 </div>
 
                 <div class="links">
